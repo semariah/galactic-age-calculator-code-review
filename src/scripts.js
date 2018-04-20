c046e061f8a275e75cba377d6aec98856438ebe7
semhar <semaria.eyzac@gmal.com>
2018-04-20 10:20:07 -0700

    testing for a fail

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,10 @@
-class Calculator {
-  constructor () {
-
-  }
-  ageInSeconds(age) {
-    let secondsInYear = (age * 31536000);
-    return secondsInYear;
-  }
-}
-export {Calculator};
+// class Calculator {
+//   constructor () {
+//
+//   }
+//   ageInSeconds(age) {
+//     let secondsInYear = (age * 31536000);
+//     return secondsInYear;
+//   }
+// }
+// export {Calculator};
