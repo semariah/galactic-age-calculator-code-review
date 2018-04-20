d9e4e1fee40e1e5c58546b8a1098e0d3cf0df002
semhar <semaria.eyzac@gmal.com>
2018-04-20 10:15:23 -0700

    test fail purposley to test it

--- spec/galactic-spec.js
+++ spec/galactic-spec.js
@@ -3,6 +3,6 @@ import {Calculator} from './../src/scripts.js';
 describe('Calculator', function(){
   it('should calculate the age of someone into seconds', function() {
     let calculator = new Calculator();
-    expect(calculator.ageInSeconds(20)).toEqual(630720000);
+    expect(calculator.ageInSeconds(20)).not.toEqual(630720000);
   });
 });
