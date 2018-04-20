733610ce9a5953d59b33150bcf8fdd65515e2c5d
semhar <semaria.eyzac@gmal.com>
2018-04-20 11:49:05 -0700

    add a describe for mercury age

--- spec/galactic-spec.js
+++ spec/galactic-spec.js
@@ -5,4 +5,17 @@ describe('Calculator', function(){
     let calculator = new Calculator();
     expect(calculator.ageInSeconds(20)).toEqual(630720000);
   });
+  // it('should calculate the seconds between two days', function(){
+  //   let calculator = new Calculator();
+  //   let day1 = date(20, 4, 2017);
+  //   let day2 = date(21, 4, 2017);
+  //   let diff = day1.getTime() - day2.getTime();
+  //   let secondDiffrence = diff/1000;
+  //   let diffrenceInSeconds = Math.abs(secondDiffrence);
+  //   expect(calculator.diffrenceInSeconds(day1, day2)).toEqual(86400);
+  // })
+  it('should calculate the age of a person in mercury years', function(){
+    let calculator = new Calculator();
+    expect(calculator.ageInMercuryYear(20)).toEqual(83);
+  });
 });
