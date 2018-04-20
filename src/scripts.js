f2f0cb5ee9e1feedc70c7f5a09bff52b142c7027
semhar <semaria.eyzac@gmal.com>
2018-04-20 11:50:07 -0700

    add class for mercury age

--- src/scripts.js
+++ src/scripts.js
@@ -2,9 +2,22 @@ class Calculator {
   constructor () {
 
   }
-  ageInSeconds(age) {
+
+  ageInSeconds(age){
     let secondsInYear = (age * 31536000);
     return secondsInYear;
   }
+
+  // diffrenceInSeconds(day1, day2){
+  //   let diff = day1.getTime() - day2.getTime();
+  //   let secondDiffrence = diff/1000;
+  //   let diffrenceInSeconds = Math.abs(secondDiffrence);
+  //   return diffrenceInSeconds;
+  // }
+  ageInMercuryYear(age){
+    let mercuryAge = (Math.floor(age/0.24));
+    return mercuryAge;
+  }
+
 }
 export {Calculator};
