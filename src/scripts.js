c74f96f6bf5b9d8440cea27d6bb7d1ca40d49d32
semhar <semaria.eyzac@gmal.com>
2018-04-20 12:04:39 -0700

    add the day diffrence between two dates in seconds using class and descrip

--- src/scripts.js
+++ src/scripts.js
@@ -8,12 +8,13 @@ class Calculator {
     return secondsInYear;
   }
 
-  // diffrenceInSeconds(day1, day2){
-  //   let diff = day1.getTime() - day2.getTime();
-  //   let secondDiffrence = diff/1000;
-  //   let diffrenceInSeconds = Math.abs(secondDiffrence);
-  //   return diffrenceInSeconds;
-  // }
+  diffrenceInSeconds(day1, day2){
+    let diff = day1.getTime() - day2.getTime();
+    let secondDiffrence = diff/1000;
+    let diffrenceInSeconds = Math.abs(secondDiffrence);
+    return diffrenceInSeconds;
+  }
+
   ageInMercuryYear(age){
     let mercuryAge = (Math.floor(age/0.24));
     return mercuryAge;
