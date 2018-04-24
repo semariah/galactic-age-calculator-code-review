f68edd1d3811b4875acf8c1241044a75de7c6c7b
semariah <semaria.eyzac@gmail.com>
2018-04-24 16:46:43 -0700

    update the code for life expectancy

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,10 @@
 export class Calculator {
-  constructor () {
+  constructor (age) {
+    this.age = age;
+    //this.secondsInYear = secondsInYear;
+    //this.mercuryAge = mercuryAge;
+    this.lifeExpectancy = 60
+
 
   }
 
@@ -36,74 +41,78 @@ export class Calculator {
     return jupiterAge;
   }
 
-  lifeExpectancyPlanet(age, stress, smoke, planet) {
-    if (stress === "high" && smoke === "yes" && planet === "mercury"){
-      const lifeExpect = 60;
-      if(age <= lifeExpect) {
-        let earthLeft = (lifeExpect - age);
-        return (Math.floor(earthLeft/0.24) + " years on " + planet);
+  lifeExpectancyPlanet(age, planet) {
+    //if (planet === "mercury"){
+      //let lifeExpectancy = this.lifeExpectancy;
+      if(age <= 60) {
+        let earthLeft = (this.lifeExpectancy - age);
+        let mercuryLeft = Math.floor(earthLeft/0.24);
+        return mercuryLeft; //(Math.floor(earthLeft/0.24) + " years on " + planet);
       } else {
         return "you are past the estimated, stay healthy!"
       }
-     } else if (stress === "low" && smoke === "no" && planet === "mercury"){
-      const lifeExpect = 100;
-      if (age <= lifeExpect) {
-        let earthLeft = (lifeExpect - age);
-         return (Math.floor(earthLeft/0.24) + " years on " + planet);
-      } else {
-         return "you are past the estimated, stay healthy!";
-       }
+     //} else if (planet === "mercury"){
+      //const lifeExpect = 100;
+      // if (age <= 100) {
+      //   let earthLeft = (lifeExpectancy - age);
+      //   let mercuryLeft = Math.floor(earthLeft/0.24);
+      //   return mercuryLeft;
+      //    //return (Math.floor(earthLeft/0.24) + " years on " + planet);
+      // } else {
+      //    return "you are past the estimated, stay healthy!";
+      //  }
 
-     } else if (stress === "high" && smoke === "yes" && planet === "venus"){
-       const lifeExpect = 60;
-       if(age <= lifeExpect) {
-        let earthLeft = (lifeExpect - age);
-         return (Math.floor(earthLeft/0.62) + " years on " + planet);
-       } else {
-         return "you are past the estimated, stay healthy!";
-       }
-     } else if (stress === "low" && smoke === "no" && planet === "venus"){
-       const lifeExpect = 100;
-      if(age <= lifeExpect) {
-        let earthLeft = (lifeExpect - age);
-        return (Math.floor(earthLeft/0.62) + " years on " + planet);
-      } else {
-        return "you are past the estimated, stay healthy!";
-      }
+    //  } else if (stress === "high" && smoke === "yes" && planet === "venus"){
+    //    const lifeExpect = 60;
+    //    if(age <= lifeExpect) {
+    //     let earthLeft = (lifeExpect - age);
+    //     let mercuryLeft = Math.floor(earthLeft/0.62
+    //      return mercuryLeft; //(Math.floor(earthLeft/0.62) + " years on " + planet);
+    //    } else {
+    //      return "you are past the estimated, stay healthy!";
+    //    }
+    //  } else if (stress === "low" && smoke === "no" && planet === "venus"){
+    //    const lifeExpect = 100;
+    //   if(age <= lifeExpect) {
+    //     let earthLeft = (lifeExpect - age);
+    //     return (Math.floor(earthLeft/0.62) + " years on " + planet);
+    //   } else {
+    //     return "you are past the estimated, stay healthy!";
+    //   }
 
-    } else if (stress === "high" && smoke === "yes" && planet === "mars"){
-      const lifeExpect = 60;
-      if(age <= lifeExpect) {
-        let earthLeft = (lifeExpect - age);
-        return (Math.floor(earthLeft/1.88) + " years on " + planet);
-      } else {
-        return "you are past the eastimated, stay healthy!"
-      }
-    } else if (stress === "low" && smoke === "no" && planet === "mars"){
-      const lifeExpect = 100;
-      if(age <= lifeExpect) {
-        let earthLeft = (lifeExpect - age);
-        return (Math.floor(earthLeft/1.88) + " years on " + planet);
-      } else {
-        return "you are past the eastimated, stay healthy!"
-      }
-
-    } else if (stress === "high" && smoke === "yes" && planet === "jupiter"){
-       const lifeExpect = 60;
-       if(age <= lifeExpect) {
-         let earthLeft = (lifeExpect - age);
-         return (Math.floor(earthLeft/11.86) + " years on " + planet);
-       } else {
-         return "you are past the eastimated, stay healthy!"
-       }
-  } else if (stress === "low" && smoke === "no" && planet === "jupiter"){
-    const lifeExpect = 100;
-    if(age <= lifeExpect) {
-      let earthLeft = (lifeExpect - age);
-      return (Math.floor(earthLeft/11.86) + " years on " + planet);
-    } else {
-      return "you are past the eastimated, stay healthy!"
-    }
-  }
+  //   } else if (stress === "high" && smoke === "yes" && planet === "mars"){
+  //     const lifeExpect = 60;
+  //     if(age <= lifeExpect) {
+  //       let earthLeft = (lifeExpect - age);
+  //       return (Math.floor(earthLeft/1.88) + " years on " + planet);
+  //     } else {
+  //       return "you are past the eastimated, stay healthy!"
+  //     }
+  //   } else if (stress === "low" && smoke === "no" && planet === "mars"){
+  //     const lifeExpect = 100;
+  //     if(age <= lifeExpect) {
+  //       let earthLeft = (lifeExpect - age);
+  //       return (Math.floor(earthLeft/1.88) + " years on " + planet);
+  //     } else {
+  //       return "you are past the eastimated, stay healthy!"
+  //     }
+  //
+  //   } else if (stress === "high" && smoke === "yes" && planet === "jupiter"){
+  //      const lifeExpect = 60;
+  //      if(age <= lifeExpect) {
+  //        let earthLeft = (lifeExpect - age);
+  //        return (Math.floor(earthLeft/11.86) + " years on " + planet);
+  //      } else {
+  //        return "you are past the eastimated, stay healthy!"
+  //      }
+  // } else if (stress === "low" && smoke === "no" && planet === "jupiter"){
+  //   const lifeExpect = 100;
+  //   if(age <= lifeExpect) {
+  //     let earthLeft = (lifeExpect - age);
+  //     return (Math.floor(earthLeft/11.86) + " years on " + planet);
+  //   } else {
+  //     return "you are past the eastimated, stay healthy!"
+  //   }
+  //}
 }
 }
